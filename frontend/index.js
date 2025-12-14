812792fb3ce3886a08d131ef3c4bcf003526fdea
ismaeelahmed24 <234476506+ismaeelahmed24@users.noreply.github.com>
2025-12-14 16:41:04 +0000

    Implement client request for food

--- frontend/index.js
+++ frontend/index.js
@@ -43,3 +43,26 @@ const makeRecentExerciseRequest = () => {
         console.log(error)
     })
 }
+
+const makeFoodSummaryRequest = () => {
+    fetch("http://localhost:8100/api/v1/food/57981/2025-12-14", {
+        method: "GET",
+        headers: {
+            'Accept': 'application/json',
+        },
+    })
+    .then((response) => {
+        if (response.status === 200){
+            return response.json();
+        }
+        else {
+            throw "error occured";
+        }
+    })
+    .then((resJson) => {
+        populateFoodSummary(resJson);
+    })
+    .catch((error) => {
+        console.log(error)
+    })    
+}
