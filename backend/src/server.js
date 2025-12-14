b587f00c639f6152349157cec97f9b4045506f32
ismaeelahmed24 <234476506+ismaeelahmed24@users.noreply.github.com>
2025-12-14 16:44:15 +0000

    Implement calorie stats query

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,3 +4,21 @@ import { Client } from 'pg'
 const app = express();
 const port = 8100;
 app.use(express.json());
+
+const computeCalorieStats = async (id, date) => {
+  const client = new Client({  
+  user: 'postgres',
+  password: 'mysecretpassword',
+  host: 'calorie_db',
+  port: 5432,
+  database: 'caloriedb',});
+
+  await client.connect();
+
+  const foodTotal = await client.query(`SELECT SUM(CalorieCount) AS totalfoodcalorie FROM food WHERE UserID = ${id} AND CAST(DiaryDate AS DATE) = '${date}'`);
+  const exerciseTotal = await client.query(`SELECT SUM(CalorieCount) AS totalexercisecalorie FROM exercise WHERE UserID = ${id} AND CAST(DiaryDate AS DATE) = '${date}'`);
+  const calorieGoal = await client.query(`SELECT CalorieGoal FROM users WHERE ID = ${id}`)
+  await client.end();
+  
+  return foodTotal.rows.concat(exerciseTotal.rows, calorieGoal.rows);
+}
