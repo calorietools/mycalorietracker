12cde6d6ff4bb7fef48b8eecf8686ea3c2425545
ismaeelahmed24 <234476506+ismaeelahmed24@users.noreply.github.com>
2025-12-14 16:40:26 +0000

    Implement client activity request

---
+++ frontend/index.js
@@ -0,0 +1,22 @@
+const makeActivityMeterRequest = () => {
+    fetch("http://localhost:8100/api/v1/caloriestats/57981/2025-12-14", {
+            method: "GET",
+            headers: {
+                'Accept': 'application/json',
+            },
+        })
+        .then((response) => {
+            if (response.status === 200){
+                return response.json();
+            }
+            else {
+                throw "error occured";
+            }
+        })
+        .then((resJson) => {
+            populateActivityMeter(resJson);
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+}
