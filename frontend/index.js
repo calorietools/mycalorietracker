14b32528363f3d1659e1ded0e6667e7fd4904ec4
ismaeelahmed24 <234476506+ismaeelahmed24@users.noreply.github.com>
2025-12-14 16:40:40 +0000

    Implement exercise client request

--- frontend/index.js
+++ frontend/index.js
@@ -20,3 +20,26 @@ const makeActivityMeterRequest = () => {
             console.log(error)
         })
 }
+
+const makeRecentExerciseRequest = () => {
+    fetch("http://localhost:8100/api/v1/exercise/57981/2025-12-14", {
+        method: "GET",
+        headers: {
+            'Accept': 'application/json',
+        },
+    })
+    .then((response) => {
+        if (response.status === 200){
+            return response.json();
+        }
+        else {
+            throw "error occured";
+        }
+    })
+    .then((resJson) => {
+        populateRecentExercise(resJson);
+    })
+    .catch((error) => {
+        console.log(error)
+    })
+}
