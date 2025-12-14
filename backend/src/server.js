5726a3810257eff40491eee7155d59dd122eb79d
ismaeelahmed24 <234476506+ismaeelahmed24@users.noreply.github.com>
2025-12-14 16:43:54 +0000

    Implement API base

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,26 +3,4 @@ import { Client } from 'pg'
 
 const app = express();
 const port = 8100;
-
-const client = new Client({  
-  user: 'postgres',
-  password: 'mysecretpassword',
-  host: 'calorie_db',
-  port: 5432,
-  database: 'caloriedb',})
-
-await client.connect()
-
-const res = await client.query('SELECT * FROM users')
-console.log(res.rows)
-await client.end()
-
-app.get('/', (req, res) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.send("RESPONSE");
-});
-
-// Start the server
-app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}`);
-}); 
+app.use(express.json());
