dd8b74861bbb19072cf955d47928cda643154dcd
ismaeelahmed24 <234476506+ismaeelahmed24@users.noreply.github.com>
2025-12-14 16:46:16 +0000

    Update database sample data

--- db/sample.sql
+++ db/sample.sql
@@ -1,11 +1,49 @@
-CREATE TABLE IF NOT EXISTS users (
- id SERIAL PRIMARY KEY,
- name VARCHAR(50),
- email VARCHAR(100) UNIQUE
+CREATE TABLE users (
+    ID int NOT NULL,
+    FirstName varchar(255),
+    LastName varchar(255),
+    Email varchar(255),
+    Age int,
+    UserWeight int,
+    CalorieGoal int,
+    PRIMARY KEY (ID)
 );
 
-INSERT INTO users (name, email) VALUES
- ('T1', 't1@example.com'),
- ('T2', 't2@example.com'),
- ('T3', 't3@example.com')
-ON CONFLICT (email) DO NOTHING;
+INSERT INTO users (id, FirstName, LastName, Email, Age, UserWeight, CalorieGoal) 
+VALUES (57981, 'Paul', 'Smith', '57981@example.com', 56, 65, 2500);
+
+CREATE TABLE food (
+    ID SERIAL,
+    FoodType varchar(255),
+    FoodName varchar(255),
+    CalorieCount int,
+    UserID int,
+    DiaryDate date,
+    PRIMARY KEY (ID),
+    FOREIGN KEY (UserID) REFERENCES users(ID)
+);
+
+INSERT INTO food (FoodType, FoodName, CalorieCount, UserID, DiaryDate) 
+VALUES 
+('breakfast', 'food 1', 120, 57981, '2025-12-14'),
+('breakfast', 'food 2', 73, 57981, '2025-12-14'), 
+('lunch', 'food 3', 75, 57981, '2025-12-14'), 
+('lunch', 'food 4', 98, 57981, '2025-12-14'), 
+('dinner', 'food 5', 166, 57981, '2025-12-14');
+
+CREATE TABLE exercise (
+    ID SERIAL,
+    ExerciseType varchar(255),
+    ExerciseName varchar(255),
+    CalorieCount int,
+    UserID int,
+    DiaryDate date,
+    PRIMARY KEY (ID),
+    FOREIGN KEY (UserID) REFERENCES users(ID)
+);
+
+INSERT INTO exercise (ExerciseType, ExerciseName, CalorieCount, UserID, DiaryDate) 
+VALUES 
+('directions_walk', 'Walk 1', 265, 57981, '2025-12-14'),
+('hiking', 'Hike 2', 587, 57981, '2025-12-14'), 
+('pool', 'Swim 3', 396, 57981, '2025-12-14');
